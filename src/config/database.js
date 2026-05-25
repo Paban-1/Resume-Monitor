f67c1e5c9f46e40089d9471237e5e34e4a78f994
Paban-blip <rpaban89@gmail.com>
2026-05-24 21:59:45 -1200

    fix some DNS isuue and added a DB connection

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+async function connectToDB() {
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log('Conected to Database')
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+module.exports = connectToDB
