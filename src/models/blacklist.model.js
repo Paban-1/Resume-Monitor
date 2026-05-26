5b99a15df4efa05084d97a78171b0284f94a71e2
Paban-blip <rpaban89@gmail.com>
2026-05-25 19:31:59 -1200

    create black list model for logout

---
+++ src/models/blacklist.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const blackListTokenSchema = new Schema({
+    token: {
+        type: String,
+        required: [true, "token is required to be added in blacklist"]
+    }
+}, { timestamps: true })
+
+const tokenBlackListModel = mongoose.model('blacklistTokens', blackListTokenSchema)
+module.exports = tokenBlackListModel
