4637768608b109d3ee2e154a49e51f8bfc04b252
Paban-blip <rpaban89@gmail.com>
2026-05-28 08:22:25 -1200

    perform twoway binding

--- FrontEnd/src/auth/pages/Register.jsx
+++ FrontEnd/src/auth/pages/Register.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, useNavigate } from "react-router"
 
 const Register = () => {
   const navigate = useNavigate()
 
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -17,17 +20,23 @@ const Register = () => {
         <form onSubmit={handleSubmit}>
           <div className="input-group">
             <label htmlFor="username">username</label>
-            <input type="text" id='username' placeholder='Crate User name' />
+            <input
+              onChange={(e) => { setUsername(e.target.value) }}
+              type="text" id='username' placeholder='Crate User name' />
           </div>
 
           <div className="input-group">
             <label htmlFor="email">Email</label>
-            <input type="email" id='email' placeholder='Enter email address' />
+            <input
+              onChange={(e) => { setEmail(e.target.value) }}
+              type="email" id='email' placeholder='Enter email address' />
           </div>
 
           <div className="input-group">
             <label htmlFor="password">Password</label>
-            <input type="password" id='password' placeholder='Enter password' />
+            <input
+              onChange={(e) => { setPassword(e.target.value) }}
+              type="password" id='password' placeholder='Enter password' />
           </div>
 
           <button className='button primary-button'>Register</button>
