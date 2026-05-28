d6dab489d15c542589a93ce7e3497a8c0e92fed6
Paban-blip <rpaban89@gmail.com>
2026-05-27 13:47:42 -1200

    added a package to solve cors error

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const cookieParser = require('cookie-parser')
-
+const cors = require('cors')
 
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
 
 // Require all the routes here
 const authRouter = require('./routes/auth.routes')
